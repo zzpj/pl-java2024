fa63edc13ba4367d09447e8ead6a4888ceb90885
snowbird <zbyszkonatkanski@gmail.com>
2024-04-16 17:45:22 +0200

    wersja zero

--- spring/src/main/java/com/zzpj/EventManager/EventManagerApplication.java
+++ spring/src/main/java/com/zzpj/EventManager/EventManagerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EventManagerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EventManagerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagerApplication.class, args);
+    }
 }
